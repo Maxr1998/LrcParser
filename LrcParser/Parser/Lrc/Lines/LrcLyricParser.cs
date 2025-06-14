c32a9706d4fee52498d609be7b263e3a0e6d67e1
Maxr1998 <max.rumpf1998@gmail.com>
2025-06-14 02:25:21 +0200

    Rework LRC parsing with word time tags
    - Only parse word time tags if there's a single line time tag
    - Correctly handle white space between word time tags
    - Support parsing time tags that surround each non-blank segment

--- LrcParser/Parser/Lrc/Lines/LrcLyricParser.cs
+++ LrcParser/Parser/Lrc/Lines/LrcLyricParser.cs
@@ -1,6 +1,7 @@
 // Copyright (c) karaoke.dev <contact@karaoke.dev>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using LrcParser.Model;
 using LrcParser.Parser.Lines;
 using LrcParser.Parser.Lrc.Metadata;
 using LrcParser.Parser.Lrc.Utils;
@@ -14,8 +15,20 @@ public class LrcLyricParser : SingleLineParser<LrcLyric>
 
     public override LrcLyric Decode(string text)
     {
-        var (startTimes, lyricText) = LrcStartTimeUtils.SplitLyricAndTimeTag(text);
-        var (lyric, timeTags) = LrcTimedTextUtils.TimedTextToObject(lyricText);
+        var (startTimes, rawLyric) = LrcStartTimeUtils.SplitLyricAndTimeTag(text);
+
+        // Word time tags can't be used if there are no or more than one line time tags
+        if (startTimes.Length is 0 or > 1)
+        {
+            return new LrcLyric
+            {
+                Text = rawLyric,
+                StartTimes = startTimes,
+                TimeTags = new SortedDictionary<TextIndex, int>()
+            };
+        }
+
+        var (lyric, timeTags) = LrcTimedTextUtils.TimedTextToObject(rawLyric, startTimes[0]);
 
         return new LrcLyric
         {
